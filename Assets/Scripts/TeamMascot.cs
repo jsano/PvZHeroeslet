5b99716a9f16d540c2ebeb8ae9a1a44c5eb58645
jsano <jasonlee0607@gmail.com>
2025-01-04 14:01:26 -0800

    Team Mascot

---
+++ Assets/Scripts/TeamMascot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamMascot : Card
+{
+
+	protected override IEnumerator OnTurnStart()
+	{
+		yield return new WaitForSeconds(1);
+		for (int col = 0; col < 5; col++) {
+			Card c = Tile.zombieTiles[0, col].planted;
+			if (c != null && c.tribes.Contains(Tribe.Sports))
+			{
+				c.RaiseAttack(1);
+				c.Heal(1, true);
+			}
+		}
+	}
+
+}
