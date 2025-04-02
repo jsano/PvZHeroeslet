2cccfa4d30efcf0e2b7460fad43b93c97ebe8022
jsano <jasonlee0607@gmail.com>
2025-04-01 19:32:23 -0700

    Zombot 1000, teamup fix

---
+++ Assets/Scripts/ZombieCards/Zombot1000.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombot1000 : Card
+{
+
+	protected override IEnumerator OnThisPlay()
+	{
+		GameManager.Instance.DisableHandCards();
+		yield return new WaitForSeconds(1);
+		for (int i = 0; i < 5; i++)
+		{
+			if (Tile.plantTiles[0, i].planted != null) Tile.plantTiles[0, i].planted.Destroy();
+            if (Tile.plantTiles[1, i].planted != null) Tile.plantTiles[1, i].planted.Destroy();
+        }
+		yield return base.OnThisPlay();
+	}
+
+}
