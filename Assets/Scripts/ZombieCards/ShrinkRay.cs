3baa423846081cc5091f725df4a0f03b1afb2b1a
jsano <jasonlee0607@gmail.com>
2025-04-09 00:07:23 -0700

    Shrink Ray, Slammin' Smackdown

---
+++ Assets/Scripts/ZombieCards/ShrinkRay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkRay : Card
+{
+	protected override IEnumerator OnThisPlay()
+	{
+		yield return new WaitForSeconds(1);
+		Tile.plantTiles[row, col].planted.RaiseAttack(-3);
+		GameManager.Instance.DrawCard(team);
+		yield return base.OnThisPlay();
+	}
+
+	public override bool IsValidTarget(BoxCollider2D bc)
+	{
+		Tile t = bc.GetComponent<Tile>();
+		if (t == null) return false;
+		if (t.planted != null && t.planted.team == Team.Plant) return true;
+		return true;
+	}
+
+}
---
+++ Assets/Scripts/ZombieCards/SlamminSmackdown.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlamminSmackdown : Card
+{
+
+	protected override IEnumerator OnThisPlay()
+	{
+		yield return new WaitForSeconds(1);
+		Tile.plantTiles[row, col].planted.Destroy();
+		yield return base.OnThisPlay();
+	}
+
+	public override bool IsValidTarget(BoxCollider2D bc)
+	{
+		Tile t = bc.GetComponent<Tile>();
+		if (t == null) return false;
+        if (t.HasRevealedPlanted() && t.planted.team == Team.Plant && t.planted.atk <= 4) return true;
+        return false;
+	}
+
+}
